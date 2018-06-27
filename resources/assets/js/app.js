7afb9ac91dac33a640342a2066bc036a42291857
aligator28 <pluta.pluta.pluta@gmail.com>
2018-06-27 16:55:24 +0300

    branch 6 app.js removed commented

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -39,57 +39,6 @@ Vue.component('sent-message', require('./components/Sent.vue'));
 
 const app = new Vue({
     el: '#app'
-
-    // data: {
-   	// 	messagesss: []
-    // },
-    // mounted() {
-		// this.fetchMessages();
-
-		// const channel = window.Echo.private('messs');
-
-		// channel
-            // .listen('.message.sent', function(e) {
-                // let classname = 'other_message';
-
-                // if (e.user.name == laravel_user.name) {
-                //     classname = 'author_message';
-                // }
-
-     //            this.messagesss.push({ 
-     //            	message: e.message.content,
-					// user: e.user
-     //            }); 
-
-                //добавляем новое сообщение
-                // sbm.removeAttr('disabled'); //"освобождаем" кнопку сабмита
-                // txtfld.val(''); //в текстовом поле чистота:)
-                // list.find('li').first().remove(); //из списка убираем сообщение, которое стоит в самом верху
-        // });
-    // },
-    // methods: {
-		// addMessage(message) {
-		// 	this.messagesss.push(message);
-
-		// 	axios.post('/messages', message)
-	 //          .then(function (response) {
-	 //            // console.log(response.data);
-	 //          })
-	 //          .catch(function (error) {
-	 //            // console.log(error);
-	 //          });
-		// },
-
-		// fetchMessages() {
-		// 	axios.get('/messages')
-	 //          .then(function (response) {
-	 //            this.messagesss = response.data;
-	 //          })
-	 //          .catch(function (error) {
-	 //            // console.log(error);
-	 //          });			
-		// }
-    // }
 });
 // 
 // import Pusher from "pusher-js"
